d06cc9469c1381517ff874a2a9af9a48ebd9ea8a
mohamedeissa3000 <107439278+mohamedeissa3000@users.noreply.github.com>
2022-06-14 02:23:44 +0200

    blink library added

---
+++ app/src/main/java/com/example/myapplication55/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication55;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.mohamed.eissa.imagetotext;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
